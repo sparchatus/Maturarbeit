d1629cafbd41f7a823a830419d61b117f88da0ab
spyspyyou <sandro.rueegge@hispeed.ch>
2015-04-09 18:13:22 +0200

    trying to setup the primitive game view

---
+++ Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/GameActivity.java
@@ -0,0 +1,34 @@
+package ch.imlee.maturarbeit.game;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ch.imlee.maturarbeit.R;
+
+public class GameActivity extends Activity {
+
+    Button particleButton;
+    Button bombButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GameView(this));
+
+        particleButton = (Button) findViewById(R.id.button_particle);
+        bombButton = (Button) findViewById(R.id.button_bomb);
+        particleButton.setOnClickListener(new View.OnClickListener(){
+            public void  onClick(View v) {
+            //save that p-button was clicked
+            }
+        });
+        bombButton.setOnClickListener(new View.OnClickListener(){
+             public void  onClick(View v) {
+            //save that b-button was clicked
+            }
+        });
+    }
+}

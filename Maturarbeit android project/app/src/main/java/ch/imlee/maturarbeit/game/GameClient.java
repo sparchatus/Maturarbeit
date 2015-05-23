6e5bde87869e8ee0424837694a59fa030923d255
spyspyyou <sandro.rueegge@hispeed.ch>
2015-05-23 14:48:52 +0200

    unimportant addings from my activity

---
+++ Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/GameClient.java
@@ -0,0 +1,26 @@
+package ch.imlee.maturarbeit.game;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class GameClient extends Activity {
+
+    /**private int screenWidth;
+    private int screenHeight;
+
+    private GameSurface surface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_the_flag);
+        Display display = getWindowManager().getDefaultDisplay();
+        screenWidth = display.getWidth();
+        screenHeight = display.getHeight();
+        surface = (GameSurface) findViewById(R.id.capture_the_flag_surface);
+        surface.setScreenDimensions(screenWidth, screenHeight);
+    }
+    */
+}

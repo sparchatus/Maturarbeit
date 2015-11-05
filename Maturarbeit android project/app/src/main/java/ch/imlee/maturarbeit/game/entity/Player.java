ee617773136323b81f9ef55f0638d90368603133
spyspyyou <sandro.rueegge@hispeed.ch>
2015-11-05 16:42:46 +0100

    Revert "logging name"
    This reverts commit 580466de074dba269cdfc2346e537a141505fbd3.

--- Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/entity/Player.java
+++ Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/entity/Player.java
@@ -15,7 +15,6 @@ import ch.imlee.maturarbeit.game.Sound.SlimeSound;
 import ch.imlee.maturarbeit.game.Sound.StunSound;
 import ch.imlee.maturarbeit.game.map.Map;
 import ch.imlee.maturarbeit.game.Tick;
-import ch.imlee.maturarbeit.utils.LogView;
 import ch.imlee.maturarbeit.views.GameSurface;
 
 /**
@@ -113,12 +112,11 @@ public class Player extends Entity implements Tick {
         float[] charWidths = new float[nameChars.length];
         float textWidth = 0;
         namePaint.getTextWidths(nameChars, 0, nameChars.length-1, charWidths);
-        namePaint.setAlpha(0xff);
-        for (float w : charWidths){
-            textWidth += w;
+        for (int i = 0; i < charWidths.length; ++i){
+            textWidth += charWidths[i];
         }
         canvas.drawText(NAME, getXCoordinate() - GameThread.getUser().getXCoordinate() - textWidth / 2, getYCoordinate() - GameThread.getUser().getXCoordinate() - 6, namePaint);
-        LogView.addLog("name of device " + getID() + ": " + NAME);
+
         return canvas;
     }
 

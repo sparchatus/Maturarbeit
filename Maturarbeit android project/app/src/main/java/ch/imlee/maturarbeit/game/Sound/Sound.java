9a39afd7aa0528f98bc1fb50001c52c1e9b36a71
Baegi <buks@hispeed.ch>
2015-12-07 19:56:33 +0100

    sound changes

--- Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/Sound/Sound.java
+++ Maturarbeit android project/app/src/main/java/ch/imlee/maturarbeit/game/Sound/Sound.java
@@ -1,8 +1,11 @@
 package ch.imlee.maturarbeit.game.Sound;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 
+import ch.imlee.maturarbeit.R;
 import ch.imlee.maturarbeit.activities.GameClient;
 
 /**
@@ -10,7 +13,7 @@ import ch.imlee.maturarbeit.activities.GameClient;
  */
 
 public class Sound {
-    //// TODO: 27.08.2015 use SoundPool instead of MediaPlayer 
+/*    //// TODO: 27.08.2015 use SoundPool instead of MediaPlayer
     protected MediaPlayer mediaPlayer;
     protected static Context context = GameClient.getContext();
 
@@ -41,4 +44,54 @@ public class Sound {
 //        mediaPlayer.stop();
 //        mediaPlayer.release();
     }
+
+    */
+
+    // from API 21 on, you should use SoundPool.Builder to create a SoundPool, but we want our app to be compatible with earlier versions on, so we use the deprecated constructor
+    private static SoundPool soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+    // the int 1 as the last element of the constructor has currently no effect. More info on
+    // http://developer.android.com/reference/android/media/SoundPool.html
+    private static int particleHit = soundPool.load(GameClient.getContext(), R.raw.particle_collision, 1);
+    private static int slime = soundPool.load(GameClient.getContext(), R.raw.slime, 1);
+    private static int stun = soundPool.load(GameClient.getContext(), R.raw.stun, 1);
+    private static int background = soundPool.load(GameClient.getContext(), R.raw.background_test_1, 1);
+
+    public static void initialize(){
+        // the -1 stands for an infinite loop
+        soundPool.setLoop(background, -1);
+        soundPool.setLoop(slime, -1);
+        soundPool.setLoop(stun, -1);
+    }
+
+    public static void play(int id){
+        soundPool.play(id, 1, 1, )
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
